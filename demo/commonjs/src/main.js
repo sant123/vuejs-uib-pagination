8e73e2c0eb93ac36253bb8ceffae78a1e39b41fc
Santiago Aguilar Hernández <santiago777@outlook.com>
2017-10-26 00:30:23 -0500

    + Added .editorconfig + NPM modules updated + JS from demo folder indented

--- demo/commonjs/src/main.js
+++ demo/commonjs/src/main.js
@@ -7,21 +7,21 @@ var app = new Vue({
     el: "#app",
     data: {
         totalItems: 64,
-        pagination1: { currentPage: 4 },
-        pagination2: { currentPage: 1 },
-
-        setPage: function(pageNo) {
+        pagination1: {
+            currentPage: 4
+        },
+        pagination2: {
+            currentPage: 1
+        },
+        setPage: function (pageNo) {
             this.pagination1.currentPage = pageNo;
         },
-
         pageLabelHtml: function (pageNo) {
-            return `<b>${pageNo}</b>`;
+            return "<b>" + pageNo + "</b>";
         },
-
-        pageChanged: function() {
+        pageChanged: function () {
             console.log('Page changed to: ' + this.pagination1.currentPage);
         },
-
         maxSize: 5,
         bigTotalItems: 175
     }
--- demo/global/dist/main.js
+++ demo/global/dist/main.js
@@ -2,27 +2,21 @@ var app = new Vue({
     el: "#app",
     data: {
         totalItems: 64,
-
         pagination1: {
             currentPage: 4
         },
-
         pagination2: {
             currentPage: 1
         },
-
         setPage: function (pageNo) {
             this.pagination1.currentPage = pageNo;
         },
-
         pageLabelHtml: function (pageNo) {
-            return `<b>${pageNo}</b>`;
+            return "<b>" + pageNo + "</b>";
         },
-
         pageChanged: function () {
             console.log('Page changed to: ' + this.pagination1.currentPage);
         },
-
         maxSize: 5,
         bigTotalItems: 175
     }
